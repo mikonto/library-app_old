5bc7e559db70b8fb05f122a83fdbcb02b29479c6
Mikael Kontolampi <mikael.kontolampi@gmail.com>
2022-08-17 15:55:42 +0200

    Add accordion form for adding new books

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
+
+
+var accordion = document.getElementById("accordion");
+var panel = document.getElementById("panel");
+panel.style.display = "none";
+
+accordion.addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    if (panel.style.display === "block") {
+        panel.style.display ="none";
+    }
+    else {
+        panel.style.display = "block";
+    }
+    
+}
+)
+
+
+
 let myLibrary = [ // store book objects into array
     {
       "author": "J.R.R. Tolkien",
