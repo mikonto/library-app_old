8751a4fa8f5439f3b72ca29b41656a15877b46c7
Mikael Kontolampi <mikael.kontolampi@gmail.com>
2022-08-18 11:57:46 +0200

    Add functionality to add & remove new books from myLibrary
    Add functionality to add new books into myLibrary.
    - Add form for adding new books into myLibrary.
    - Add table generator, that creates/updates html table based
        on myLibrary array.
    - Add delete function, to delete specific books from
        myLibrary array.

--- main.js
+++ main.js
@@ -1,85 +1,67 @@
-// var acc = document.getElementsByClassName("accordion");
-// var i;
+var accordion = document.getElementById("accordion"); // new book button
+var panel = document.getElementById("panel"); // input fields for new book
+panel.style.display = "none"; // input fields for new book are hidden by default
 
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-//   });
-// }
-
-
-var accordion = document.getElementById("accordion");
-var panel = document.getElementById("panel");
-panel.style.display = "none";
-
-accordion.addEventListener("click", function() {
-    this.classList.toggle("active");
-
-    if (panel.style.display === "block") {
+accordion.addEventListener("click", function() { // clicking new book button opens input fields
+    this.classList.toggle("active"); // adds class "active" -> changes button text to "close"
+    if (panel.style.display === "block") { // input fields visible/hidden
         panel.style.display ="none";
     }
     else {
         panel.style.display = "block";
     }
-    
+})
+
+var addBtn = document.getElementById("add-btn"); // add button to add new book
+addBtn.addEventListener("click", function() { // when add button is clicked 
+    var author = document.getElementById("author").value; // content in input fields are put into variables
+    var title = document.getElementById("title").value;
+    var pages = document.getElementById("pages").value;
+    var read = document.getElementById("read").checked;
+    var obj = new Book(author, title, pages, read); // call object constructor
+    myLibrary.push(obj); // push new object into array myLibrary
+    createTable() // recreate table to include new object
+    clearFields(); // clear input fields
+})
+
+function clearFields() { // clear input fields
+    document.getElementById("author").value = "";
+    document.getElementById("title").value = "";
+    document.getElementById("pages").value = "";
+    document.getElementById("read").checked = false;
 }
-)
 
+let myLibrary = [] // array for book objects, empty by default
 
-
-let myLibrary = [ // store book objects into array
-    {
-      "author": "J.R.R. Tolkien",
-      "Title": "Lord of the Rings",
-      "Number of pages": "1178",
-      "Read": true,
-    },
-    {
-        "author": "Douglas Adams",
-        "Title": "The Hitchhiker's Guide to the Galaxy",
-        "Number of pages": "208",
-        "Read": true,
-    },
-    {
-        "author": "Kalle Päätalo",
-        "Title": "Ihmisiä Telineillä",
-        "Number of pages": "488",
-        "Read": false,
-    },
-]
-
-function Book(author, title, pageAmount, read) { // construct new object
+function Book(author, title, pages, read) { // construct new object
     this.author = author;
     this.title = title;
-    this.pageAmount = pageAmount;
+    this.pages = pages;
     this.read = read;
 }
 
-function addBookToLibrary(author, title, pageAmount, read) { // add new book into library
-  const obj = new Book(author, title, pageAmount, read); // call object constructor
-  myLibrary.push(obj); // push new object into array myLibrary
-  createTable() // recreate table to include new object
-
-}
-
 function createTable() { // create HTML table from myLibrary array
-    const table = document.getElementById('table'); // new variable table = table in HTML
-    clearTable(table); // clear table before adding myLibrary into it (so the same books dont appear in the table multiple times)
-
+    const table = document.getElementById('table'); // new variable for table
+    clearTable(table); // clear table before adding myLibrary into it (prevent duplicate books)
+    let rowId = 0; // variable that gives a unique id for each row (used in delete.png icon)
     for(const obj of myLibrary){ // loop over array myLibrary and create table
-        const row = document.createElement('tr');
-        for(const val of Object.values(obj)){
-            const col = document.createElement('td');
-            col.textContent = val;
-            row.appendChild(col);
+        const row = document.createElement('tr'); // create row variable
+        for(const val of Object.values(obj)){ // loop through values of each object
+            const col = document.createElement('td'); // create cell variable
+            col.textContent = val; // add value into cell
+            row.appendChild(col); // add cell into row
         }
-        table.appendChild(row);
+        const deleteCell = document.createElement('td'); // last cell for each row (container for deleteImg)
+        row.appendChild(deleteCell); // add cell into row
+        
+        const deleteImg = document.createElement("img"); // create img element (img is used as delete row button)
+        deleteImg.classList.add("delete-img"); // add class "delete-img" into img element
+        deleteImg.src = "images/delete.png"; // add src into img element
+        deleteImg.id = rowId // give unique id for each img element based on row id
+        deleteCell.appendChild(deleteImg); // add img into cell
+
+        rowId++ // change row id +1 before looping into next row
+        table.appendChild(row); // add row into table
     }
 }
 
@@ -88,24 +70,13 @@ function clearTable(table) { // clear HTML table
     while (--i) {
         table.deleteRow(i);
     }
-  }
+}
 
-createTable(); // create HTML table when page loads
-
-
-
-
-
-
-// function Book(title, author, pageNum, read) {
-//     this.title = title
-//     this.author = author
-//     this.pageNum = pageNum
-//     this.read = read
-//     this.info = function() {
-//     return `${title} by ${author}, ${pageNum} pages, ${read} yet`;
-//         }
-//     }
-
-//     const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read')
-//     console.log(theHobbit.info());
+document.body.addEventListener("click", function (event) { // when clicking body..
+    if (event.target.classList.contains("delete-img")) { // .. function checks if element with class "delete-img" is clicked (delete icon on each row)
+        var id = event.target.getAttribute("id"); // store unique delete img id into id variable
+    const objectToBeRemoved = myLibrary[id]; // use unique delete img id to determine which object in array is to be removed 
+    myLibrary.splice(myLibrary.findIndex(a => a === objectToBeRemoved) , 1) // remove object from array
+    createTable() // update HTML table after array has been modified
+    }
+});
